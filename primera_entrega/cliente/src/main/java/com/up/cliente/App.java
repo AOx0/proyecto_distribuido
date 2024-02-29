1a5b6438fc8b6fda84a9a3c79d9c30a5b412f6b4
Alejandro Osornio <aoxo.contact@gmail.com>
2024-02-28 20:51:26 -0600

    Init

--- primera_entrega/cliente/src/main/java/com/up/cliente/App.java
+++ primera_entrega/cliente/src/main/java/com/up/cliente/App.java
@@ -1,13 +1,26 @@
 package com.up.cliente;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws UnknownHostException, IOException
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Conectando a 127.0.0.1:31303");
+        Socket socket = new Socket("127.0.0.1", 31303);
+        System.out.println("Conectado!");
+
+        Scanner socket_in = new Scanner(socket.getInputStream());
+        System.out.println("Mensaje recibido: " + socket_in.nextLine());
+
+        socket_in.close();
+        socket.close();
     }
 }
--- primera_entrega/nodo/src/main/java/com/up/nodo/App.java
+++ primera_entrega/nodo/src/main/java/com/up/nodo/App.java
@@ -1,13 +1,49 @@
 package com.up.nodo;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+class Cliente {
+    InetAddress addr;
+    int port;
+
+    public Cliente(Socket socket) {
+        this.addr = socket.getInetAddress();
+        this.port = socket.getPort();
+    }
+
+    public String toString() {
+        return this.addr + ":" + this.port;
+    }
+
+}
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        HashMap<Cliente, Socket> conexiones = new HashMap<Cliente, Socket>(); 
+
+        ServerSocket server = new ServerSocket(31303);
+
+        while (true) {
+            Socket client = server.accept();
+            Cliente info_cliente = new Cliente(client);
+            
+            System.out.println("Nueva conexión de " + info_cliente);
+
+            System.out.println("Mandando mensaje 'Hola desde el server!'");
+            OutputStream out = client.getOutputStream();
+
+            out.write("Hola desde el server!\n".getBytes());
+
+            conexiones.put(info_cliente, client);
+        }
     }
 }
