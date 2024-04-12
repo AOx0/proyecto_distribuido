f830a6e9197b14372bf62ef0f751903dad6dda6c
Alejandro Osornio <aoxo.contact@gmail.com>
2024-04-12 09:18:30 -0600

    Revert "Format"
    This reverts commit 5e8c16b6fbfc5e0540a15bec33d21f9e554a13fe.

--- primera_entrega/nodo/src/main/java/com/up/nodo/App.java
+++ primera_entrega/nodo/src/main/java/com/up/nodo/App.java
@@ -8,42 +8,42 @@ import java.net.Socket;
 import java.util.HashMap;
 
 class Cliente {
-        InetAddress addr;
-        int port;
+    InetAddress addr;
+    int port;
 
-        public Cliente(Socket socket) {
-                this.addr = socket.getInetAddress();
-                this.port = socket.getPort();
-        }
+    public Cliente(Socket socket) {
+        this.addr = socket.getInetAddress();
+        this.port = socket.getPort();
+    }
 
-        @Override
-        public String toString() {
-                return this.addr + ":" + this.port;
-        }
+    @Override
+    public String toString() {
+        return this.addr + ":" + this.port;
+    }
 
 }
 
 public class App 
 {
-        public static void main( String[] args ) throws IOException
-        {
-                HashMap<Cliente, Socket> conexiones = new HashMap<Cliente, Socket>(); 
+    public static void main( String[] args ) throws IOException
+    {
+        HashMap<Cliente, Socket> conexiones = new HashMap<Cliente, Socket>(); 
 
-                ServerSocket server = new ServerSocket(31010);
-                System.out.println("Servidor escuchando en 127.0.0.1:31010");
+        ServerSocket server = new ServerSocket(31010);
+        System.out.println("Servidor escuchando en 127.0.0.1:31010");
 
-                while (true) {
-                        Socket client = server.accept();
-                        Cliente info_cliente = new Cliente(client);
-                        
-                        System.out.println("Nueva conexión de " + info_cliente);
+        while (true) {
+            Socket client = server.accept();
+            Cliente info_cliente = new Cliente(client);
+            
+            System.out.println("Nueva conexión de " + info_cliente);
 
-                        System.out.println("Mandando mensaje 'Hola desde el server!'");
-                        OutputStream out = client.getOutputStream();
+            System.out.println("Mandando mensaje 'Hola desde el server!'");
+            OutputStream out = client.getOutputStream();
 
-                        out.write("Hola desde el server!\n".getBytes());
+            out.write("Hola desde el server!\n".getBytes());
 
-                        conexiones.put(info_cliente, client);
-                }
+            conexiones.put(info_cliente, client);
         }
+    }
 }
--- segunda_entrega/nodo/src/main/java/com/up/ConfigBuilder.java
+++ segunda_entrega/nodo/src/main/java/com/up/ConfigBuilder.java
@@ -9,77 +9,77 @@ import java.util.Vector;
 import java.util.stream.Stream;
 
 public class ConfigBuilder {
-                                public static Config parseFromFileConfig(String ruta) throws ConfigError, FileNotFoundException, IOException {
-                                                                try (BufferedReader bin = new BufferedReader(new FileReader(ruta));) {
-                                                                                                return ConfigBuilder.parseConfig(bin.lines());
+                public static Config parseFromFileConfig(String ruta) throws ConfigError, FileNotFoundException, IOException {
+                                try (BufferedReader bin = new BufferedReader(new FileReader(ruta));) {
+                                                return ConfigBuilder.parseConfig(bin.lines());
+                                }
+                }
+        
+                public static Config parseConfig(Stream<String> contents) throws ConfigError {
+                                final boolean collect_error[] = new boolean[] { false };
+                                final int line_num[] = new int[] { 0 };
+
+                		HashMap<String, HashMap<String, Vector<String>>> config = new HashMap<String, HashMap<String, Vector<String>>>();
+                		String section[] = new String[] { null };
+                                
+                                contents
+                                                .map(String::trim)
+                                                .forEach(line -> {
+                                                                line_num[0]++;
+
+                                                                if (line.isEmpty() || line.isBlank()) return;
+                                                                if (
+                                                                                line.startsWith("[") 
+                                                                                && line.endsWith("]") 
+                                                                                && line.chars().filter(c -> c == '[').count() == 1 
+                                                                                && line.chars().filter(c -> c == ']').count() == 1
+                                                                ) {
+                                                                                section[0] = line.replaceFirst("\\[", "").replaceFirst("]", "").trim();
+                                                                                config.put(section[0], new HashMap<String, Vector<String>>());
+                                                                                return;
                                                                 }
-                                }
-                
-                                public static Config parseConfig(Stream<String> contents) throws ConfigError {
-                                                                final boolean collect_error[] = new boolean[] { false };
-                                                                final int line_num[] = new int[] { 0 };
 
-                                		HashMap<String, HashMap<String, Vector<String>>> config = new HashMap<String, HashMap<String, Vector<String>>>();
-                                		String section[] = new String[] { null };
-                                                                
-                                                                contents
-                                                                                                .map(String::trim)
-                                                                                                .forEach(line -> {
-                                                                                                                                line_num[0]++;
+                                                                long num_eq = line.chars().filter(x -> x == '=').count();
+                                                                if (num_eq != 1) {
+                                                                                System.err.println(
+                                                                                                                "Error en la línea " + line_num[0] + " de la configuración:\n"
+                                                                                                                                                + " " + line);
+                                                                                collect_error[0] = true;
+                                                                                return;
+                                                                }
 
-                                                                                                                                if (line.isEmpty() || line.isBlank()) return;
-                                                                                                                                if (
-                                                                                                                                                                line.startsWith("[") 
-                                                                                                                                                                && line.endsWith("]") 
-                                                                                                                                                                && line.chars().filter(c -> c == '[').count() == 1 
-                                                                                                                                                                && line.chars().filter(c -> c == ']').count() == 1
-                                                                                                                                ) {
-                                                                                                                                                                section[0] = line.replaceFirst("\\[", "").replaceFirst("]", "").trim();
-                                                                                                                                                                config.put(section[0], new HashMap<String, Vector<String>>());
-                                                                                                                                                                return;
-                                                                                                                                }
+                                                                String[] keyval = line.split("=");
 
-                                                                                                                                long num_eq = line.chars().filter(x -> x == '=').count();
-                                                                                                                                if (num_eq != 1) {
-                                                                                                                                                                System.err.println(
-                                                                                                                                                                                                                                "Error en la línea " + line_num[0] + " de la configuración:\n"
-                                                                                                                                                                                                                                                                                                + " " + line);
-                                                                                                                                                                collect_error[0] = true;
-                                                                                                                                                                return;
-                                                                                                                                }
+                                                                String key = keyval[0].trim().replace("\"", "");
+                                                                Vector<String> values = new Vector<String>();
+                                                                if (keyval[1].contains("[")) {
+                                                                                String vals[] = keyval[1]
+                                                                                                .replace("[", "")
+                                                                                                .replace("]", "")
+                                                                                                .split(",");
 
-                                                                                                                                String[] keyval = line.split("=");
+                                                                                        for (String value : vals) {
+                                                                                                        values.add(value.trim().replace("\"", ""));
+                                                                                        }
+                                                                        } else {
+                                                                                        values.add(keyval[1].trim().replace("\"", ""));
+                                                                        }
 
-                                                                                                                                String key = keyval[0].trim().replace("\"", "");
-                                                                                                                                Vector<String> values = new Vector<String>();
-                                                                                                                                if (keyval[1].contains("[")) {
-                                                                                                                                                                String vals[] = keyval[1]
-                                                                                                                                                                                                .replace("[", "")
-                                                                                                                                                                                                .replace("]", "")
-                                                                                                                                                                                                .split(",");
-
-                                                                                                                                                                                for (String value : vals) {
-                                                                                                                                                                                                                values.add(value.trim().replace("\"", ""));
-                                                                                                                                                                                }
-                                                                                                                                                } else {
-                                                                                                                                                                                values.add(keyval[1].trim().replace("\"", ""));
-                                                                                                                                                }
-
-                                                                                                                                if (section[0] == null) {
-                                                                                                                                                System.out.println(
-                                                                                                                                                                "Error: no hay definida una sección en la linea " + line_num[0] + ":\n"
-                                                                                                                                                                                                                                                                                                + " " + line
-                                                                                                                                                );
-                                                                                                                                }
-                                                                                                                                config.get(section[0]).merge(key, values, (curr, past) -> {
-                                                                                                                                                                curr.addAll(past);
-                                                                                                                                                                return curr;
-                                                                                                                                });
-                                                                                });
+                                                                if (section[0] == null) {
+                                                                        System.out.println(
+                                                                                "Error: no hay definida una sección en la linea " + line_num[0] + ":\n"
+                                                                                                                                                + " " + line
+                                                                        );
+                                                                }
+                                                                config.get(section[0]).merge(key, values, (curr, past) -> {
+                                                                                curr.addAll(past);
+                                                                                return curr;
+                                                                });
+                                        });
 
 
-                                                                if (collect_error[0]) throw new ConfigError();
-                                                                return new Config(config);
-                                }
-                
+                                if (collect_error[0]) throw new ConfigError();
+                                return new Config(config);
+                }
+        
 }
